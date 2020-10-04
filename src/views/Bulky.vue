f72031d0c5c7ae3ad793c9c1f7615163377a46c0
Adam Whitlock <awhitlock@dealerinspire.com>
2020-10-03 21:56:33 -0600

    add and remove boxes

--- src/views/Bulky.vue
+++ src/views/Bulky.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <div id="bulky"></div>
+    <div class="flex items-center justify-start">
+      <button
+        class="px-4 py-2 m-2 text-white bg-green-700 rounded"
+        @click="addBox"
+      >
+        Add Extra Box
+      </button>
+      <button
+        v-if="this.added.length > 0"
+        class="px-4 py-2 m-2 text-white bg-green-700 rounded"
+        @click="removeBox"
+      >
+        Remove Extra Box
+      </button>
+      <p class="mr-2">extra boxes: {{ added.length }}</p>
+    </div>
   </div>
 </template>
 
@@ -9,7 +25,8 @@ import p5 from 'p5'
 export default {
   data() {
     return {
-      bulky: null
+      bulky: null,
+      added: [{ x: 100, y: 10000, z: 50 }]
     }
   },
   mounted() {
@@ -22,22 +39,39 @@ export default {
       s.draw = () => {
         s.background(0)
         s.noStroke()
-        s.frameRate(15)
+        s.frameRate(30)
         s.fill(50, 150, 50, 50)
         s.rotateX(s.frameCount * 0.01)
         s.rotateY(s.frameCount * 0.01)
-        s.box(200, 200, 100)
-        s.box(100, 200, 200)
-        s.rotateX(s.frameCount * -0.01)
-        s.rotateY(s.frameCount * -0.01)
-        s.box(100, 10000, 50)
-        s.rotateX(s.frameCount * -0.01)
-        s.rotateY(s.frameCount * -0.01)
-        s.box(10000, 100, 50)
-        s.fill(s.random(255), s.random(255), s.random(255))
+        s.box(200, 200, 200)
+
+        if (this.added.length > 0) {
+          this.added.map((item) => {
+            s.box(item.x, item.y, item.z)
+            s.rotateX(s.frameCount * -0.01)
+            s.rotateY(s.frameCount * -0.01)
+          })
+        }
       }
     }
     this.bulky = new p5(sketch)
+  },
+  methods: {
+    addBox() {
+      this.added.push({
+        x: this.randInt(200),
+        y: this.randInt(10000),
+        z: this.randInt(200)
+      })
+    },
+    removeBox() {
+      if (this.added.length > 0) {
+        this.added.pop()
+      }
+    },
+    randInt(max) {
+      return Math.floor(Math.random() * Math.floor(max))
+    }
   }
 }
 </script>
