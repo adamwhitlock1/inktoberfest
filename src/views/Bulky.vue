1ef3e30afbb96fdff71d5e1a8faa6f3f23004a21
Adam Whitlock <awhitlock@dealerinspire.com>
2020-10-03 21:23:39 -0600

    bulky

--- src/views/Bulky.vue
+++ src/views/Bulky.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
     <p>Bulky coming soon..</p>
+    <div id="bulky"></div>
   </div>
 </template>
 
 <script>
-export default {}
+import p5 from 'p5'
+export default {
+  data() {
+    return {
+      bulky: null
+    }
+  },
+  mounted() {
+    const sketch = (s) => {
+      s.setup = () => {
+        let bulky = s.createCanvas(500, 500, s.WEBGL)
+        bulky.parent('bulky')
+      }
+
+      s.draw = () => {
+        s.background(0)
+        s.noStroke()
+        s.frameRate(15)
+        s.fill(50, 150, 50, 50)
+        s.rotateX(s.frameCount * 0.01)
+        s.rotateY(s.frameCount * 0.01)
+        s.box(200, 200, 100)
+        s.box(100, 200, 200)
+        s.rotateX(s.frameCount * -0.01)
+        s.rotateY(s.frameCount * -0.01)
+        s.box(100, 10000, 50)
+        s.rotateX(s.frameCount * -0.01)
+        s.rotateY(s.frameCount * -0.01)
+        s.box(10000, 100, 50)
+        s.fill(s.random(255), s.random(255), s.random(255))
+      }
+    }
+    this.bulky = new p5(sketch)
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/Wisp.vue
+++ src/views/Wisp.vue
@@ -1,11 +1,100 @@
 <template>
   <div>
-    <p>Wisp coming soon...</p>
+    <canvas id="wisp" resize class="bg-gray-200 w-full"></canvas>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      message: 'hey',
+      name: 'Effie',
+      age: 9
+    }
+  },
+  mounted() {
+    const wisp = document.getElementById('wisp')
+    this.$paper.setup(wisp)
+    let path
+    this.$paper.view.onMouseDown = (event) => {
+      path = new this.$paper.Path()
+      path.add(event.point)
+      path.strokeColor = 'red'
+    }
+
+    this.$paper.view.onMouseDrag = (event) => {
+      path.add(event.point)
+    }
+
+    this.$paper.view.onMouseUp = () => {
+      this.processPath(path)
+    }
+  },
+  methods: {
+    processPath(path) {
+      console.log({ path })
+      var values = {
+        scale: 0.5,
+        smooth: 0.5,
+        maxrep: 25,
+        minlength: 10,
+        autorotate: true,
+        rotate: -90
+      }
+
+      // get frequently used params
+      var scale = values.scale
+      // HACK: transform can be upset by scale = 1 and angle = 90, 45 etc
+      if (scale === 1) {
+        scale = 0.9999
+      }
+      var autorotate = values.autorotate
+      var rotate = values.rotate
+      var minlength = values.minlength
+      var maxrep = values.maxrep
+
+      // smooth the hand drawn curve
+      path.smooth({ type: 'catmull-rom', factor: values.smooth })
+
+      var nextseg = path.clone()
+      console.log({ nextseg })
+      for (var i = 0; i < maxrep; i++) {
+        var lastCurve = path.lastCurve
+        console.log({ lastCurve })
+        var point2 = lastCurve.point2
+        var angle2 = lastCurve.getTangentAt(1).angle
+        console.log({ angle2 })
+
+        nextseg.scale(scale)
+
+        var firstB = nextseg.curves[0]
+        var point1 = firstB.point2
+        console.log({ firstB: firstB.getTangentAt(1) })
+        var angle1 = firstB.getTangentAt(1).getAngle()
+
+        var angle
+        if (autorotate) {
+          angle = angle2 - angle1
+        } else {
+          angle = rotate
+        }
+
+        nextseg.rotate(angle, point1)
+
+        nextseg.position += point2 - point1
+
+        if (nextseg.length < minlength) {
+          break
+        } else {
+          path.join(nextseg.clone())
+        }
+      } // loop repetitions
+
+      nextseg.remove() // remove the working segment
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
